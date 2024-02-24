46aa73f7048c7a2e3bad25e6afa5f342019ea1c2
PiShrestha <pratikstha980@gmail.com>
2024-02-24 13:23:27 -0500

    Update
    adding Hayden

--- CS.java
+++ CS.java
@@ -3,5 +3,6 @@ public class CS{
     public static void main(String[] args){
         System.out.println("Hello World");
         System.out.println("Jonathan");
+        System.out.println("Hayden");
     } 
 }
